b73805e9a1426e37c7f50db0612edfb7778354da
andyh <andy.980f@gmail.com>
2017-02-04 13:42:37 -0600

    WFE works, SerialUSB doesn't send until a newline is pushed at it.

--- arduino/due/due.ino
+++ arduino/due/due.ino
@@ -35,35 +35,45 @@ const InterruptPin<redLight, button1.number, CHANGE> redirq;
 const InterruptPin<greenLight, button2.number, CHANGE> greenirq;
   
 #include "polledtimer.h"
-
-class Flasher: public CyclicTimer {
-  void onDone(void) override {
-    CyclicTimer::onDone();
-    lamp.toggle();
-  }
-} flashLamp;
-
-#include "tableofpointers.h"
-RegisterTimer(flashLamp);
-
-
 extern "C" int sysTickHook(){
   PolledTimerServer();//all of our millisecond stuff hangs off of here.
   return false; //allowed standard code to run
 }
 
+class Flasher: public CyclicTimer {
+  using CyclicTimer::CyclicTimer;
+  void onDone(void) override {
+    CyclicTimer::onDone();
+    lamp.toggle();
+  }
+} flashLamp(451);
+
+#include "tableofpointers.h"
+RegisterTimer(flashLamp);
+
+CyclicTimer noncritical(1250);
+RegisterTimer(noncritical);
+
+CyclicTimer shorttimer(250);
+RegisterTimer(shorttimer);
+
 void setup() {
   SerialUSB.begin(230400);//'native' usb port
-  SerialUSB.print("Playing hooky\n");
+  SerialUSB.println("Playing hooky\n");
   Serial.begin(115200);//an actual uart
   //Pin structs take care of themselves, unless you need special modes outside arduino's libraries.
   greenirq.attach(true);//we don't build in attach() to the constructor as in many cases the isr needs stuff that isn't initialized until setup() is run.
-  redirq.attach();
-  flashLamp.restart(789);
+  redirq.attach(true);
 }
+
 
 char indicator='-';
 void loop() {
-  __WFE();
-  Serial.write(indicator);
+  __WFE();//this made the beat between the two timers go away, which is a good thing.
+  if(shorttimer.hasFired()){
+    SerialUSB.print(indicator);
+  }
+  if(noncritical.hasFired()){
+    SerialUSB.println();
+  }
 }
