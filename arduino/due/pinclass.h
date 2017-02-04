2f5433b0e8d09cf0aeecd22e951b6ea95ac88223
andyh <andy.980f@gmail.com>
2017-02-04 12:25:34 -0600

    some cleanup for when compiled outside of arduino

--- arduino/due/pinclass.h
+++ arduino/due/pinclass.h
@@ -1,13 +1,19 @@
 #pragma once
 
 #ifndef ARDUINO
-#define HIGH 1
 #define LOW 0
-#define INPUT_PULLUP 1
-#define OUTPUT 0
-void pinMode(unsigned, unsigned);
-bool digitalRead(unsigned);
-void digitalWrite(unsigned, bool);
+#define HIGH 1
+
+#define INPUT 0
+#define OUTPUT 1
+#define INPUT_PULLUP 2
+#define CHANGE 2 
+#define FALLING 3
+#define RISING 4
+
+void pinMode(unsigned pinnum, unsigned PINMODE);
+bool digitalRead(unsigned pinnum);
+void digitalWrite(unsigned pinnum, bool);
 #endif
 /* @param arduinoNumber is such as 13 for the typical LED pin.
 @param mode should be one of:: INPUT(0), INPUT_PULLUP(2), and OUTPUT(1) 
@@ -15,17 +21,18 @@ void digitalWrite(unsigned, bool);
 Via polarity you can adde an inverting buffer to a line and change that in one place. 
 Also if driving a FET to operate a higher voltage load a HIGH on a pin is usually a LOW on the device.
 
-Since this is template code the compiler should generate the traditional line of arduino code, these should have no overhead whatsoever compared to useing the digitalXXX calls explicitly.
+Since this is template code the compiler should generate the traditional line of arduino code, these should have no overhead whatsoever compared to using the digitalXXX calls explicitly.
 
 The pinMode() command which is usually in the setup() gets called where declared. 
 For statically declared pins (not inside a function) this executes shortly before setup() via the c-startup code. 
-This means that statically declared Pins canbe used in your setup() code.
+This means that statically declared Pins can be used in your setup() code.
 If you declare a Pin in a function then the pinMode command executes each time you enter the function.
 
-The downside of a template based implementation is that each Pin is a class unto itself. That means you can't reasonable pass a reference to one to a routine such as a software serial port. 
-There is a number() method added so that you can pass that piece of into about the pin to standard arduino code.
+The downside of a template based implementation is that each Pin is a class unto itself. That means you can't pass a reference to one to a routine such as a software serial port. 
+There is a number() method added so that you can pass that piece of into about the pin to standard arduino code, or to your code which creates a pin locally which does a potentially pointless pinMode() but otherwise works fine.
 
 */
+
 /** pretending to not know that HIGH=1 and LOW=0 ... constexpr should inline '1-active' at each place of use and not actually do a function call */
 static constexpr bool inverse(bool active){
   return HIGH+LOW-active;
@@ -70,7 +77,7 @@ template <unsigned arduinoNumber,unsigned mode,unsigned polarity=HIGH> struct Pi
  *  Also note that some devices have more options such as pulldown, that arduino does not provide access to.
  */
 template <unsigned arduinoNumber,unsigned polarity=HIGH> struct InputPin: public Pin<arduinoNumber, INPUT_PULLUP, polarity>{
-  operator bool()const {
+  operator bool() const {
     return Pin<arduinoNumber, INPUT_PULLUP, polarity>::get();
   }
 };
