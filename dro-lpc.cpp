a1fc768c9c6d23899942e20211fa35d9ce301bb2
andyh <andy.980f@gmail.com>
2017-02-20 17:24:22 -0600

    updating for library changes, not yet compiling

--- dro-lpc.cpp
+++ dro-lpc.cpp
@@ -1,104 +1,83 @@
 /* dro project
- * first user of lpcbase so building it in that directory, will later split into project and library.
+ * this attempt is with lpc1343, using Olimex P1343 dev kit (8 LED's :)
  */
+#include "quadraturecounter.h"
+QuadratureCounter pos;//as in 'position'
 
 #include "lpc13xx.h"
-#include "gpio.h" // lpcexpresso
-#include "nvic.h" // alh
-#include "core_cmInstr.h" // cm3 intrinsics for WFE
-#include "cheapTricks.h"
-//#include "functional"
-#include "systick.h" //periodic behavior
-//#include "P1343devkit.h" //for led's
+#include "gpio.h" 
+#include "nvic.h" 
 
+//shared ~millisecond services:
+#include "systick.h"
+using namespace SystemTimer;
+#include "polledtimer.h"
+#include "tableofpointers.h"
+
+CyclicTimer ledToggle(750);//todo: clearer timebase
+RegisterTimer(ledToggle);
+
+#include "core_cmInstr.h"  //wfe OR wfi
+#include "cruntime.h"
+
+#include "p1343_board.h" //P1343devkit //for led's
+#include "wtf.h"
 using namespace LPC;
 
 //should inherit from a board description header
 const unsigned ExpectedClock=12000000;
-// p0-4,p05 for qei.
 
+// using p0-4,p05 for qei.
 const InputPin<PortNumber(0), BitNumber(4)> primePhase(BusLatch);
 const InputPin<0, 5> otherPhase(BusLatch);
 
-Irq qeiPrimeIrq(4);
+Irq<4> qeiPrimeIrq;
 
-int axis;
 // prime phase interrupt
-HandleInterrupt(4){ // interrupt number and bit number coincidentally the same. Need to add mechanism for this.
-  bool dirbit = otherPhase;
+HandleInterrupt(4){ // interrupt number and bit number coincidentally the same. 
+  pos.step<true>(primePhase==otherPhase);
+}
 
-  if(dirbit) {
-    ++axis; // ignoring quarter phase for now
+/////////////{
+#include "fifo.h"
+static FifoBuffer<33> outgoing;
+static FifoBuffer<63> incoming;
+
+/** called by isr on an input event.
+ * negative values of @param are notifications of line errors, -1 for interrupts disabled */
+bool uartReceiver(int indata){
+  if((incoming=u8(indata))){
+    return true;
   } else {
-    --axis; // will be +/-4 here
+    wtf(24);
+    return false;
   }
 }
 
-// p2-11, p2-10 for index and eot.
-/**configuration:
- * polarities for each physical input
- * reverse direction
- */
-
-
-//#include "limitedpointer.h"
-//class BufferedSender {
-//  LimitedPointer <const char>pointer;
-//public:
-//  void setMessage(const char *message,int sizeofMessage){
-//    pointer=LimitedPointer <const char>(message,sizeofMessage);
-//  }
-
-//  /** called by isr when transmission becomes possible.
-//  *  @return either an 8 bit unsigned character, or -1 to disable transmission events*/
-//  int next(){
-//    return pointer?pointer.next():-1;
-//  }
-//};
-//BufferedSender UartMessage;
-
-#include "fifo.h"
-unsigned char messageBuf[256];
-Fifo ogmessage(256,messageBuf);
-
-int nextDebugChar(){
-  return ogmessage.remove();//conveniently aligned exceptional return value
+/** called by isr when transmission becomes possible.
+ *  @returns either an 8 bit unsigned character, or -1 to disable transmission events*/
+int uartSender(){
+  return outgoing;//negative for fifo empty
 }
 
 const char testMessage[]="Jello Whirled\r\n";
 
-
-int wtf(int complaint){
-  static int lastWtf = 0;
-  static int repeats = 0;
-
-  if(complaint) {
-    if(changed(lastWtf, complaint)) {
-      repeats = 0;
-    } else {
-      ++repeats;
-    }
-  }
-  return complaint;
-} // wtf
-
 #include "uart.h"
+Uart theUart(&uartReceiver,&uartSender);
+
 
 P1343devkit kit;
-CyclicTimer ledToggle;
+//CyclicTimer ledToggle;
 
 int main(void){
-  //enable debug output:
-  theUart.setTransmitter(nextDebugChar);
   theUart.setBaud(115200,ExpectedClock);
 
-  axis = 0;
   // interrupt defaults to edge sensitive
   qeiPrimeIrq.enable();
 
 
   startPeriodicTimer(1000);//shoot for millisecond resolution
-  ledToggle.restart(0.75);//seconds
+  ledToggle.trigger();//seconds
   while(1) {
     __WFE();//WFE is more inclusive than WFI, events don't call an isr but do wakeup the core.
     if(ledToggle){
