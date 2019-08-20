aa3f2e954910374d9133b47f28b905a2472137aa
980f <andy.980f@gmail.com>
2019-08-19 23:39:55 -0500

    managed to get clion and qt to agree on cmake usage

--- dro-stm.cpp
+++ dro-stm.cpp
@@ -1,3 +1,4 @@
+#pragma ide diagnostic ignored "hicpp-signed-bitwise"
 /* dro project
  */
 
@@ -20,40 +21,6 @@ static ClockStarter startup InitStep(InitHardware/2) (true,0,1000);//external wa
 //however the objects have the same usage syntax so only declarations need to be conditional on vendor.
 #include "exti.h"  //interrupts only tangentially coupled to i/o pins.
 #include "bluepill.h" //declares a board.
-
-
-static Pin buttonPin(PA,9);
-
-static const Irq &pushButton(Exti::enablePin(buttonPin,false,true));
-
-void IrqName(6) (void){
-  board.toggleLed();
-  Exti::clearPending(buttonPin);
-}
-
-static Pin primePin(PB,1);
-const InputPin primePhase(primePin);
-static Pin otherPin(PB,2);
-const InputPin otherPhase(otherPin);
-
-static const Irq &prime(Exti::enablePin(primePin,true,true));
-
-#define myIrq 40
-
-//should go up and down depending upon the input signals;
-static int axis(0);
-
-HandleInterrupt( myIrq ) {
-  //prime phase interrupt
-  //void QEI (void) {
-  bool dirbit = otherPhase;
-  if (dirbit) {
-    --axis; //ignoring quarter phase for now
-  } else {
-    ++axis; //will be +/-4 here
-  }
-  Exti::clearPending(primePin);
-}
 
 #include "fifo.h"
 
@@ -82,7 +49,6 @@ int uartSender(){
   return outdata;//negative for fifo empty
 }
 
-//LPC
 //void prepUart(){
 //  theUart.setFraming("N81");
 //  theUart.setBaud(115200);
@@ -95,28 +61,20 @@ int uartSender(){
 
 #include "minimath.h"
 
-//LPC specific:
-//#include "packdatatest.h"
-//static packdatatest ender  __attribute((section(".rodata.packdata.last"))) ={'\0'};
 
-
-int main(void) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+int main() {
 //  prepUart();
   CyclicTimer slowToggle(ticksForSeconds(3)); //since action is polled might as well wait until main to declare the object.
   //soft stuff
   int events=0;
-  //  Exti::enablePin(otherPin);
 
-  prime.enable();
-  pushButton.enable();
-
-  while (1) {
-    stackFault();//useless here, present to test compilation.
+  while (true) {
     //re-enabling in the loop to preclude some handler shutting them down. That is unacceptable, although individual ones certainly can be masked.
-    IrqEnable=1;//master enable
-    MNE(WFI);//wait for interrupt. Can't get a straight answer from arm on whether WFE also triggers on interrupts.
+    IrqEnable=true;//master enable
+    MNE(WFE);//wait for interrupt. Can't get a straight answer from arm on whether WFE also triggers on interrupts.
     ++events;
-    board.led=bool(buttonPin);
     if(slowToggle.hasFired()){
       board.toggleLed();
       if(outgoing.insert('A'+(events&15))){
@@ -124,5 +82,4 @@ int main(void) {
       }
     }
   }
-  return 0;
 }
--- dro.cmake
+++ dro.cmake
@@ -1,7 +1,13 @@
 
-# ST device macro
-SET (DEVICE "STM32F103xx")
-#todo: set up link to irqs file, invoke builder as needed.
+#slowly splitting the cmake from this file which will hopefully be sharable someday, and CMakeLists.txt.
+
+#kill warning: qt sets this when it launches cmake, but we don't produce QT stuff so we got a warning.
+unset(QT_QMAKE_EXECUTABLE)
+
+## ST device macro
+#SET (DEVICE "STM32F103xx")
+##todo: set up link to irqs file, invoke builder as needed.
+#ADD_DEFINITIONS(-D${DEVICE})
 
 # Magic settings. Without it CMake tries to run test programs on the host platform, which fails of course.
 SET (CMAKE_SYSTEM_NAME Generic)
@@ -10,12 +16,11 @@ SET (CMAKE_SYSTEM_PROCESSOR arm)
 SET(CMAKE_C_FLAGS "-mcpu=cortex-m3 -std=c11 -fdata-sections -ffunction-sections -Wall" CACHE INTERNAL "c compiler flags")
 SET(CMAKE_CXX_FLAGS "-mcpu=cortex-m3 -std=c++14 -fno-rtti -fno-exceptions -Wall -fdata-sections -ffunction-sections -MD -Wall" CACHE INTERNAL "cxx compiler flags")
 
-
 #todo: see if we can drop these, they are in the GCC tree.
 INCLUDE_DIRECTORIES(${SUPPORT_FILES})
+#needed for soft floating point:
 LINK_DIRECTORIES(${SUPPORT_FILES})
 
-ADD_DEFINITIONS(-D${DEVICE})
 
 # project include paths .
 INCLUDE_DIRECTORIES(".")
@@ -23,10 +28,8 @@ INCLUDE_DIRECTORIES("cortexm")
 INCLUDE_DIRECTORIES("cortexm/stm32")
 INCLUDE_DIRECTORIES("ezcpp")
 
-SET (LINKER_SCRIPT "dro-stm.ld")
-
-#target_link_libraries(-lgcc)
+SET (LINKER_SCRIPT "${PROJECT_NAME}.ld")
 
 #todo: try to eliminate -specs=nosys.specs, we are suppressing routines that are referenced therein but will never execute.
-#todo: try to find a varialbe to provide /d/work/pro, the official one craters the compiler detect.
-SET (CMAKE_EXE_LINKER_FLAGS "-L /d/work/dro -T ${LINKER_SCRIPT} -nostartfiles  -specs=nosys.specs -Wl,--gc-sections,-Map,../dro/dro.map " CACHE INTERNAL "exe link flags")
+#todo: try to find a variable to provide /d/work/pro, the official one craters the compiler detect.
+SET (CMAKE_EXE_LINKER_FLAGS "-L /d/work/dro -T ${LINKER_SCRIPT} -nostartfiles -specs=nosys.specs -Wl,--gc-sections,-Map,${PROJECT_NAME}.map " CACHE INTERNAL "exe link flags")
